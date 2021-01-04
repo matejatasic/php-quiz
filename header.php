7765c85863f68e1e201c899830679da449490820
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-01-04 10:50:36 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Quiz App</title>
+
+    <!-- Bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- Custom css -->
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <!-- header -->
+    <header>
+        <div class="container text-center">
+            <h1 class="py-5">PHP Quiz</h1>
+        </div>
+    </header>
+    <!-- !header -->
+
+    <!-- main -->
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mx-auto mt-5">
