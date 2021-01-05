c3c39e92ec9f2b787513bfde9067537dc958e477
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-01-05 15:28:23 +0100

    Update final.php

--- final.php
+++ final.php
@@ -2,13 +2,14 @@
     include('header.php');
     session_start();
 ?>
-    <div class="bordered-card p-4">
+    <div class="bordered-card p-4 mb-3">
         <h2 class="text-center mb-4">Congratulations, you finished the test!</h2>
         <p>Your score is: <?php echo $_SESSION['score'] ?></p>
         <p class="mt-4 text-center">Click the button below to try again.</p>
-        <a href="questions.php?num=1" class="btn btn-primary w-25 d-block mx-auto">Try again</a>
+        <a href="index.php" class="btn btn-primary w-25 d-block mx-auto">Try again</a>
+        <p class="mt-4 text-center">If you want to add a question to this quiz, click the button below.</p>
+        <a href="add_question.php" class="btn btn-info w-25 d-block mx-auto">Add question</a>
     </div>
-    <?php session_unset() ?>
 <?php
     include('footer.php');
 ?>
